29f9e9a96cd90429cd338eec941b3d81fdf0c6a5
Best By <Best By@DESKTOP-Q9612MS>
2024-09-24 21:03:59 +0300

    CreateS2

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace WebApplication1
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World");
+            app.MapGet("/", () => "Hello Worldd");
 
             app.Run();
         }
